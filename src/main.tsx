42ac661ec0cf8b56c4706fa229a2b0b0fa3f3cb9
tonyma163 <mamanto163@gmail.com>
2024-09-11 00:13:25 +0800

    ERROR-FIXED installed buffer

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,21 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const manifestUrl = "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
+import { Buffer } from 'buffer';
+//window.Buffer = Buffer || Buffer ;
+window.Buffer = Buffer;
+/*
+import * as buffer from "buffer";
+
+if (typeof(window as any).global === "undefined") {
+  (window as any).global = window;
+}
+
+if (typeof(window as any).Buffer === "undefined") {
+  (window as any).Buffer = buffer.Buffer;
+}
+*/
+
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import path from "path"
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import inject from '@rollup/plugin-inject'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true,
+    }),
+    NodeModulesPolyfillPlugin(),
+    inject({
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
-  }
-})
+  },
+});
